3b5ba02912e6a151d27408657f68eebfc21b4aba
Samidh <samidhtalsania@gmail.com>
2013-05-03 15:03:31 +0530

    Added remove_duplicate method

--- apriori.java
+++ apriori.java
@@ -1,17 +1,16 @@
-import java.util.*;
 import java.io.*;
 
 
 class apriori{
 
-  static int count = 1 ;
+	static int count = 1 ;
 
 	//arrays that will hold item frequent set along with count
-	static ArrayList<String> table = new ArrayList<String>() ;
-	static ArrayList<Integer> frequency = new ArrayList<Integer>() ;
+	static ArrayList<String> table = new ArrayList<>() ;
+	static ArrayList<Integer> frequency = new ArrayList<>() ;
 	
-	static ArrayList<String> temp = new ArrayList<String>() ;
-	static ArrayList<String> item = new ArrayList<String>();
+	static ArrayList<String> temp = new ArrayList<>() ;
+	static ArrayList<String> item = new ArrayList<>();
 	static int itemsNum = 0 ;
 
 
@@ -30,22 +29,20 @@ class apriori{
 		//123
 		//12
 		//1
-		
+		int x = 0 , y = 0 ;
 		do{
+			x = table.size() ;
 			generateItemset() ;
 
-			// for (int i=0;i<table.size() ;i++ ) {
-			// System.out.println(table.get(i) +"  "+frequency.get(i)) ;
-			// }
+			for (int i=0;i<table.size() ;i++ ) {
+			System.out.println(table.get(i) +"  "+frequency.get(i)) ;
+			}
 
 			pruneItemset() ;
-
-			if (table.size() == 1) {
-
-				break ;
-				
-			}
-		}while(table.size() != 0);
+			y = table.size() ;
+			System.out.println(x+"-------------------#################--------------------------"+y) ;
+			
+		}while(  table.size() != 1  );
 
 		for (int i=0;i<table.size() ;i++ ) {
 
@@ -58,11 +55,11 @@ class apriori{
 
 	public static void getItems()throws IOException{
 		
-			Scanner sc = new Scanner(System.in) ;
+			
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in)) ;
 		
 			System.out.println("Enter the number of transactions:") ;
-			itemsNum = sc.nextInt();
+			itemsNum = Integer.parseInt(br.readLine()) ;
 		
 		
 			System.out.println("enter items in each transactions for "+itemsNum+" items") ;
@@ -82,30 +79,31 @@ class apriori{
 				//arrray that holds broken down string elements
 				char individualItem[] ;
 
-			
-				if (count==1) {
 				
+				if (count==1) {
+					
 					for (int i=0;i<itemsNum ;i++ ) {
-				 	
-				 		//break down itemlist into individual item ex) 1,2,3 to [1][2][3]
+					 	
+					 		//break down itemlist into individual item ex) 1,2,3 to [1][2][3]
 
-					 	String temp = item.get(i) ;
-					 	individualItem = temp.toCharArray() ;
-				 	
-					 	for (int j=0;j<individualItem.length ;j++ ) {
+						 	String temp = item.get(i) ;
+						 	individualItem = temp.toCharArray() ;
+					 	
+						for (int j=0;j<individualItem.length ;j++ ) {
 
-					 		check(table,individualItem[j]) ;
-				 		
-				 	}
-				 }
+						 		check(table,individualItem[j]) ;
+					 		
+					 	}
+					 }
 
-				 count++ ;
-			 }
+					 count++ ;
+					
+				}
 
-			//generate frequent itemsets
-			else{
-					join() ;
-			} 
+				//generate frequent itemsets
+				else{
+						join() ;
+				} 
 
 	}
 
@@ -136,7 +134,7 @@ class apriori{
 		for (int i=table.size()-1; i>=0 ; i-- ) {
 
 			int supportVal = frequency.get(i) ;
-			if (supportVal <= 2) {
+			if (supportVal < 2) {
 				
 				table.remove(i) ;
 				frequency.remove(i) ;
@@ -146,7 +144,7 @@ class apriori{
 
 		// for (int i=0;i<table.size() ;i++ ) {
 
-		// 	System.out.println(table.get(i) +"  "+frequency.get(i)) ;
+		// 	System.out.println(table.get(i) +"  "+frequency.get(i)+"-->pit") ;
 			
 		// 	}
 		
@@ -158,7 +156,7 @@ class apriori{
 	public static void join(){
 		String first ;
 		String second ;
-
+		
 		for (int i=0;i<table.size()-1  ; i++) {
 
 			first = table.get(i) ;
@@ -167,21 +165,21 @@ class apriori{
 
 				second = table.get(j);
 				String tempstr = first + second ;
+				
 				tempstr = checkUnique(tempstr) ;
+				
+				// System.out.println(tempstr+"	"+tempstr.length()+"-->join") ;
 				temp.add(tempstr);
-
+				// System.out.println(tempstr+"	"+tempstr.length()+"-->join") ;
 
 			}
 
 
 		}
-
-		//clear the table so the values can be copied from the temp table which has the new datasets
 		table.clear() ;
 		table.addAll(temp);
-		//clear the temp table 
 		temp.clear() ;
-		//clear the frequency table to calculate new frequency for new datasets
+		remove_duplicate() ;
 		frequency.clear();
 
 		//initializing all elements of frequency table to zero
@@ -190,114 +188,117 @@ class apriori{
 			frequency.add(new Integer(0)) ;
 			
 		}
-
-		// for (int i=0;i<table.size() ;i++ ) {
-
-		// 	System.out.println(table.get(i) +"  "+frequency.get(i)) ;
-			
-		// 	}
-
-		//calculating new frequencies
-
+		
 		calculate_freq() ;
 	}
 
-	public static void calculate_freq(){
-
-		
-
-		for (int i=0;i<table.size() ; i++ ) {
-
-			//retrieve string from table to counts its frequency
-			String getTablevalue = table.get(i) ;
-			
-			char tableValue[] ;
-			tableValue = getTablevalue.toCharArray() ;
-			
-			int finalCount = 0 ;
-
-
-			for(int k=0 ; k<item.size() ; k++){
-
-				String itemTempValue = item.get(k) ;
-
-				int tempCount = 0 ;
-				
-
-				for (int j=0 ;j<tableValue.length ;j++ ) {
-					// String temp = Character.toString(tableValue[j]) ;
-					
-					
-
-					for (int l=0 ; l<itemTempValue.length() ; l++) {
-						
-						if( tableValue[j] == itemTempValue.charAt(l) ){
-							tempCount++ ;
-							
-							break ;
-						}
-					}
-
-
-				}
-
-				if (tempCount == tableValue.length) {
-					finalCount++ ;
-				}
-
-
-			} 
-
-			//finalCount stores the value of the total number of occurences of // a particular frequentset in the dataset
-			//store the value of finalCount in frequency
-
-			frequency.set(i,finalCount) ;
-			 
-
-			
-		}
-	}
 	
+
 	public static String checkUnique(String temp) {
 
 
 			char tempStr[] = temp.toCharArray() ;
-			char tempChar[] = new char[tempStr.length] ;
-
 			//   12131213
-			tempChar[0] = tempStr[0] ;
-			int count = 1 ;
-			char x;
-
 			for (int i=0 ; i<tempStr.length-1 ;i++ ) {
 				
-				if (tempStr[i] != '0') {
-						
+				for (int j=i+1 ; j<tempStr.length ; j++ ) {
 					
-					for (int j=i+1 ; j<tempStr.length ; j++ ) {
-						
-						
+					// if (tempStr[j] == '0') {
+					// 	break ;
+					// }
 
-						if ( tempStr[i] == tempStr[j] ) {
-							tempStr[j] = '0' ;
-						}
+					if ( tempStr[i] == tempStr[j] ) {
+						tempStr[j] = '-' ;
 					}
-
-					
 				}
 			}
 
-			
+			temp = new String(tempStr) ;
+			String split[] = temp.split("-") ;
+			temp = "";
+			for (String s : split ) {
 
-			for (int i=0,j=0 ; i<tempStr.length ;i++ ) {
-				if (tempStr[i] != '0') {
-					tempChar[j++] = tempStr[i] ;
-				}
+				temp += s ;
+				
 			}
-
-			temp = new String(tempChar) ;
 
 			return temp ;
 		}
+
+
+		public static void calculate_freq(){
+				
+				int c = 0 ;
+
+				for ( String  tableVal : table ) {
+						
+						
+						int tempFrequency = 0 ;
+						
+						for ( String itemVal : item ) {
+							int tempCount = 0 ;
+							for ( char ch : tableVal.toCharArray() ){
+
+									
+
+									if ( itemVal.contains(Character.toString(ch)) ) {
+											tempCount++ ;									
+									}
+							}
+
+							
+							
+							
+							if ( tempCount == tableVal.length() ) {
+									tempFrequency++ ;
+
+							}	
+						}
+				
+						
+
+						
+						frequency.set(c,tempFrequency) ;
+						c++ ;
+				}
+
+				
+		
+		}
+
+		public static void remove_duplicate(){
+						
+			for ( int i=0 ; i<table.size() ; i++ ) {
+		 		char strArray[] = table.get(i).toCharArray() ;
+		 		
+
+		 		int sum_first = 0 ;
+		 		
+		 		for ( int j=0 ; j<strArray.length ; j++ ) {
+		 			sum_first += Integer.parseInt(Character.toString(strArray[j])) ;
+		 		}
+
+		 		// System.out.println(sum_first) ;
+
+		 		for ( int j=table.size()-1 ; j>i ; j-- ) {
+		 			
+		 			int sum_second = 0 ;
+		 			char strArray_second[] = table.get(j).toCharArray() ;
+		 			
+		 			for ( int k=0 ; k<strArray_second.length ; k++ ) {
+		 				sum_second += Integer.parseInt(Character.toString(strArray_second[k])) ;
+		 			}
+                                          
+		 			if (sum_first == sum_second) {
+						// System.out.println("removed:"+table.get(j)+"by:"+table.get(i)) ;
+		 				table.remove(j) ;
+		 			}
+
+		 		}
+
+		 	}	
+		}
+		
+	
 }
 
